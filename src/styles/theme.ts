9dc2ebb75ce376804821438071f20a3cd6b8e32f
Morathii <morathii@icloud.com>
2022-06-14 18:22:22 +0200

    💄 Reset default browser CSS and add simple theme (#16)

---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+type Color = {
+    main: string
+    light: string
+    dark: string
+    text: string
+}
+
+type ColorsMatrix = {
+    primary: Color
+    secondary: Color
+}
+
+export const colors: ColorsMatrix = {
+    primary: {
+        main: '#6667ab',
+        light: '#9695dd',
+        dark: '#373d7b',
+        text: '#ffffff',
+    },
+    secondary: {
+        main: '#00ba96',
+        light: '#5bedc7',
+        dark: '#008968',
+        text: '#000000',
+    },
+}
