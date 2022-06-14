66dcc0291f6b2ea1a6e767b3c8eb5a731a73e7a4
Morathii <morathii@icloud.com>
2022-06-14 18:20:56 +0200

    ✨ User slice (#16)

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { useDispatch as useDispatchRtk } from 'react-redux'
 
-import settingsReducer from './settingsSlice'
+import userReducer from './userSlice'
 
 export const store = configureStore({
     reducer: {
-        settings: settingsReducer,
+        user: userReducer,
     },
 })
+
+export type Dispatch = typeof store.dispatch
+export const useDispatch = () => useDispatchRtk<Dispatch>()
