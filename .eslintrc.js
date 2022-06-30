a82cee6e5b2b61780ba361118c2369467308ec74
Sebastan Weber <sebastian.weber@diva-e.com>
2022-06-30 19:47:40 +0200

    🧑‍💻 Add react hooks ESLint plugin #8

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+    extends: [
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaFeatures: {
