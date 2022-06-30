0a13e757821552449b180e6e76898e9c27721895
Morathii <morathii@icloud.com>
2022-06-30 16:51:59 +0200

    🐛 Set subtitle to being h2 instead of h1 #8

--- src/components/Subtitle.tsx
+++ src/components/Subtitle.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import { DefaultProps } from '../types/props'
 
-export const Subtitle = ({ children }: DefaultProps) => <h1>{children}</h1>
+export const Subtitle = ({ children }: DefaultProps) => <h2>{children}</h2>
